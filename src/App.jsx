52a0bd383409674bedf283e685f80318b3fb60ba
Synister <82519402+SynisterSage@users.noreply.github.com>
2025-08-31 10:31:36 -0400

    vercel data updates

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import Settings from "./pages/Settings.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";                 // ⬅️ new
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
-import { inject } from "@vercel/analytics";
-inject();
+
 export default function App() {
   return (
     <AppProvider>
